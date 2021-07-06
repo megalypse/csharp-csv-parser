9ddbd3980b314af05f1acf13d6686877f5311104
Bruno Alliguieri <alliguierisilva@hotmail.com>
2021-07-06 19:33:25 -0300

    Annotations folder name changed to CustomAttributes

---
+++ CsvParser/CustomAttributes/ColumnTargetAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CsvParser.Annotations
+{
+    public class ColumnTargetAttribute : Attribute
+    {
+        
+        public int columnNumber;
+
+        public ColumnTargetAttribute(int columnNumber)
+        {
+            this.columnNumber = columnNumber;
+        }
+    }
+}
