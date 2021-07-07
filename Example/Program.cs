dc12409d952b9257657b534dde2a811ffdd137aa
Bruno Alliguieri <alliguierisilva@hotmail.com>
2021-07-06 21:16:35 -0300

    git tracked files reseted

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,7 +1,7 @@
-﻿using CsvParser.CsvObjectParser;
-using Example.Models;
+﻿using Example.Models;
 using System.Diagnostics;
 using System.Collections.Generic;
+using CsvParser.CsvObjectParser;
 using CsvParser;
 
 namespace Example
@@ -26,7 +26,7 @@ namespace Example
                 {
                     ShouldSkipHeader = true,
                     ShouldSkipEqualObject = false
-            });
+                });
 
             Debug.Assert(personList.Count.Equals(2));
             Debug.Assert(personList[0].Name.Equals("john"));
@@ -35,18 +35,20 @@ namespace Example
             Debug.Assert(personList[1].Age.Equals("19"));
 
             List<TargetedPerson> targetedPersonList = extractor.Parse<TargetedPerson>(
-                csvString, 
+                csvString,
                 new ExtractOptions
-                { 
+                {
                     ShouldSkipHeader = true,
-                    ShouldSkipEqualObject = false 
-            });
+                    ShouldSkipEqualObject = false
+                });
 
             Debug.Assert(targetedPersonList.Count.Equals(2));
             Debug.Assert(targetedPersonList[0].Name.Equals("john"));
             Debug.Assert(targetedPersonList[0].Age.Equals("18"));
             Debug.Assert(targetedPersonList[1].Name.Equals("johanne"));
             Debug.Assert(targetedPersonList[1].Age.Equals("19"));
+
+            System.Console.WriteLine("Everything ok!");
         }
     }
 }
