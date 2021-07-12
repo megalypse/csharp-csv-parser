8998a34540c06586db95d2799437bfd0bba2c580
Bruno Alliguieri <alliguierisilva@hotmail.com>
2021-07-12 13:58:59 -0300

    Parser private methods moved to it's own service file.

--- CsvParser/CsvService.cs
+++ CsvParser/CsvService.cs
@@ -1,12 +1,56 @@
-﻿using System;
+﻿using CsvParser.Exceptions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CsvParser
 {
-    class CsvService
+    static class CsvService
     {
+        public static void CheckOptions<T>(ExtractOptions options)
+        {
+            if (options.ShouldSkipEqualObject is true)
+            {
+                List<Type> typeInterfaces = typeof(T).GetInterfaces().ToList();
+                bool containsEquatable = typeInterfaces.Contains(typeof(IEquatable<T>));
+
+                if (containsEquatable is false)
+                {
+                    string errorMessage = "Class must implement System.IEquatable interface if ShouldRepeat is False.";
+
+                    throw new InterfaceNotFoundException(errorMessage);
+                }
+            }
+        }
+
+        public static List<string> BreakLine(string separator, string line) =>
+            Regex.Split(line, ColumnSeparatorRgx(separator)).ToList();
+
+        public static List<string> BreakCsv(string csvString) =>
+            csvString.Split("\n").ToList();
+
+        public static bool ShouldSkipLoop(bool isFirstLoop, bool haveHeader) =>
+            isFirstLoop && haveHeader;
+
+        public static PropertyInfo GetPropertyOrThrow<T>(List<PropertyInfo> propertyList, string desiredName)
+        {
+            PropertyInfo property = propertyList
+                        .Where(x => x.Name.Equals(desiredName))
+                        .FirstOrDefault();
+
+            if (property is null)
+                throw new Exception($"Type ${typeof(T)} doesn't contain {desiredName} property.");
+
+            return property;
+        }
+
+        public static List<PropertyInfo> GetTypePropertyInfoList(Type type) => type.GetProperties().ToList();
+
+        static string ColumnSeparatorRgx(string separator) => 
+            $"{separator}(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
     }
 }
